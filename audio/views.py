c501ed952443d8c395bb2a13cc7a08d06d73691f
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2022-02-19 17:15:15 +0900

    create get_zoom_meeting method

--- audio/views.py
+++ audio/views.py
@@ -9,6 +9,11 @@ def input(request):
 
 def submit(request):
     meeting_id = request.POST['meeting_id']
+    meeting = get_zoom_meeting(meeting_id)
+    return HttpResponse("Your meeting is %s" % meeting)
+
+
+def get_zoom_meeting(meeting_id):
     conn = http.client.HTTPSConnection("api.zoom.us")
     headers = {
         'authorization': "Bearer JWT",
@@ -17,4 +22,4 @@ def submit(request):
     conn.request("GET", "/v2/meetings/%s" % meeting_id,  headers=headers)
     res = conn.getresponse()
     data = res.read()
-    return HttpResponse("Your meeting is %s" % data.decode("utf-8"))
+    return data.decode("utf-8")
