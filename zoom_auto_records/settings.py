208379f257209295fb30a83f8719e48bc8e60561
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2022-02-20 17:22:01 +0900

    startapp user

--- zoom_auto_records/settings.py
+++ zoom_auto_records/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'user.apps.UserConfig',
     'audio.apps.AudioConfig',
     'django.contrib.admin',
     'django.contrib.auth',
