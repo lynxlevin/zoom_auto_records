2a5bd1cf511955c27469e8e366c903389ddb9f1b
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2022-02-20 17:57:42 +0900

    ユーザー登録機能

--- user/urls.py
+++ user/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 app_name = 'user'
 urlpatterns = [
-    path('login/', views.login, name='login'),
-    path('signup/', views.signup, name='signup'),
+    path('', views.index, name='index'),
+    path('signup/', views.SignUpView.as_view(), name='signup'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,9 +1,23 @@
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from django.contrib.auth import login
+
+from user.forms import SignUpForm
 
 
-def login(request):
-    return render(request, 'user/login.html')
+def index(request):
+    return render(request, 'user/index.html')
 
 
-def signup(request):
-    return render(request, 'user/signup.html')
+class SignUpView(CreateView):
+    form_class = SignUpForm
+    template_name = 'user/signup.html'
+    success_url = reverse_lazy('audio:index')
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        self.object = user
+        return HttpResponseRedirect(self.get_success_url())
