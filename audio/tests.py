0551083153e1bfe52155207c979b19c9223d89fc
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2022-02-20 13:53:53 +0900

    unit test delete_file

--- audio/tests.py
+++ audio/tests.py
@@ -1,3 +1,15 @@
+import os
 from django.test import TestCase
 
+from audio.views import delete_file
+
 # Create your tests here.
+
+
+class AudioViewsMethodsTests(TestCase):
+    def test_delete_file(self):
+        fake_path = 'audio/fake_file.txt'
+        with open(fake_path, 'w'):
+            pass
+        delete_file(fake_path)
+        self.assertFalse(os.path.isfile(fake_path))
