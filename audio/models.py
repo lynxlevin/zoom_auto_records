99a2ca4751cf6292c5790d9cd397bee5119c8cb0
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2022-02-20 13:53:25 +0900

    delete unneeded file and code

--- audio/models.py
+++ audio/models.py
@@ -4,6 +4,3 @@ from django.db import models
 class AudioFile(models.Model):
     file = models.FileField(upload_to='audio/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.file.name
