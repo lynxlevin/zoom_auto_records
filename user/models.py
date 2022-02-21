4d446ad9936838fd812fe48500992fd864372c61
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2022-02-21 11:06:16 +0900

    add zoom_code and zoom_access_token columns to CustomUser

--- user/models.py
+++ user/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 # Create your models here.
+
+
+class CustomUser(AbstractUser):
+    zoom_code = models.CharField(max_length=255, blank=True)
+    zoom_access_token = models.CharField(max_length=255, blank=True)
+    pass
+
+
+class CustomUserManager(BaseUserManager):
+    pass
